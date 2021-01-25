c61d0ab48e441f8f37e659690005d4f730bb08fe
siwoo <siwoo8948@gmail.com>
2021-01-25 16:00:58 +0900

    index 업데이트

---
+++ team4_project/src/main/java/pack/model/ReviewDto.java
@@ -0,0 +1,74 @@
+package pack.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ReviewDto {
+	private int review_id, goods_id, review_viewCount, review_asc, review_answer;
+	private String user_id, review_title, review_content;
+	private float review_star;
+	private boolean review_isPrivate;
+	
+	public int getReview_id() {
+		return review_id;
+	}
+	public void setReview_id(int review_id) {
+		this.review_id = review_id;
+	}
+	public int getGoods_id() {
+		return goods_id;
+	}
+	public void setGoods_id(int goods_id) {
+		this.goods_id = goods_id;
+	}
+	public int getReview_viewCount() {
+		return review_viewCount;
+	}
+	public void setReview_viewCount(int review_viewCount) {
+		this.review_viewCount = review_viewCount;
+	}
+	public int getReview_asc() {
+		return review_asc;
+	}
+	public void setReview_asc(int review_asc) {
+		this.review_asc = review_asc;
+	}
+	public int getReview_answer() {
+		return review_answer;
+	}
+	public void setReview_answer(int review_answer) {
+		this.review_answer = review_answer;
+	}
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+	public String getReview_title() {
+		return review_title;
+	}
+	public void setReview_title(String review_title) {
+		this.review_title = review_title;
+	}
+	public String getReview_content() {
+		return review_content;
+	}
+	public void setReview_content(String review_content) {
+		this.review_content = review_content;
+	}
+	public float getReview_star() {
+		return review_star;
+	}
+	public void setReview_star(float review_star) {
+		this.review_star = review_star;
+	}
+	public boolean isReview_isPrivate() {
+		return review_isPrivate;
+	}
+	public void setReview_isPrivate(boolean review_isPrivate) {
+		this.review_isPrivate = review_isPrivate;
+	}
+	
+	
+}
---
+++ team4_project/src/main/webapp/index.jsp
@@ -0,0 +1,24 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+    <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+ 
+<!--   
+	index에서 구현해야할 기본 내용  
+	- jquery 이미지 슬라이드   
+	- 상품 추천, 이벤트 소식, 알뜰상품(시작할때 무작위 4개 상품 가져오기)
+	- 설 얼리버드 특가 레이아웃  
+	- (4개 무작위 상품추천과 같음)
+	
+	
+	추가내용 
+	- 설 얼리버드 특가 레이아웃에 시간 줄어드는거 표시 
+ -->
+</body>
+</html>
