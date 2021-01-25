dbea659c1ecb4534c7063b53dee11acffae09431
jangjungkeon <jkit13@naver.com>
2021-01-25 17:52:59 +0900

    지난 target 파일(.gitignore로 앞으로는 인식 안될듯)

--- team4_project/src/main/java/pack/controller/GoodsListController.java
+++ team4_project/src/main/java/pack/controller/GoodsListController.java
@@ -1,10 +1,11 @@
 package pack.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import pack.model.GoodsDaoInter;
@@ -17,10 +18,14 @@ public class GoodsListController {
 	
 	@RequestMapping(value = "goodsList", method=RequestMethod.GET)
 	public ModelAndView productList() {
+		List<GoodsDto> list = goodsDaoInter.
+		
+		ModelAndView view = new ModelAndView("jikwonlist");
+		view.addObject("list", list);
 		
 		/* 
 		 * 상품 리스트 모두 가져오기
-		 * productList에 뿌리기 
+		 *  
 		 */
 		
 		ModelAndView mav = new ModelAndView("goodsList");	
