2badd4fc472ee6c39e4c55b501118a92e93b0845
jangjungkeon <jkit13@naver.com>
2021-02-01 16:14:20 +0900

    작업 즁

--- team4_project/src/main/java/pack/model/OrdersDaoImpl.java
+++ team4_project/src/main/java/pack/model/OrdersDaoImpl.java
@@ -1,5 +1,25 @@
 package pack.model;
 
-public class OrdersDaoImpl {
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.support.SqlSessionDaoSupport;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+import pack.controller.OrdersBean;
+
+@Repository
+public class OrdersDaoImpl extends SqlSessionDaoSupport implements OrdersDaoInter{
+	 
+	@Autowired
+	public OrdersDaoImpl(SqlSessionFactory factory) {
+		setSqlSessionFactory(factory);
+	}
+	
+	@Override
+	public Boolean insertOrders(OrdersBean bean) {
+		int re = getSqlSession().insert("", bean);
+		if (re == 1) return true;
+		else return false;
+	}
+	
 }
